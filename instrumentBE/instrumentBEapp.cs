d11a8603de7355e4734cca6be1097983e2ce9645
OlavioH <olavio.holm@gmail.com>
2023-01-30 20:30:37 +0100

    Added baseline to get user input for connections to FE and dataDB

---
+++ instrumentBE/instrumentBEapp.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+int FE_tcp = 0;
+int FE_ip  = 0;
+
+int DataDB_tcp = 0;
+int DataDB_ip  = 0;
+
+
+// Establish connection with instrumentFE
+Console.WriteLine("instrumentFE connection:\n");
+Console.WriteLine("-------------------------\n");
+
+Console.WriteLine("TCP port:");
+FE_tcp = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("IP address:");
+FE_ip = Convert.ToInt32(Console.ReadLine());
+
+
+// Establish connection with instrumentDataDB
+Console.WriteLine("instrumentDataDB connection:\n");
+Console.WriteLine("-------------------------\n");
+
+Console.WriteLine("TCP port:");
+DataDB_tcp = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("IP address:");
+DataDB_ip = Convert.ToInt32(Console.ReadLine());
