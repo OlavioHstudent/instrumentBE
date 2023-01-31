fb45d8004ee90314be4e1616b4147dcfe3f97a51
OlavioH <olavio.holm@gmail.com>
2023-01-31 22:02:41 +0100

    Created a listener, and successfully established a connection between instrumentFE and instrumentBE. It is also possible to cancel the listener by pressing ESC.

--- instrumentBE/instrumentBEapp.cs
+++ instrumentBE/instrumentBEapp.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using static System.Net.Mime.MediaTypeNames;
+
 
 // Variables
 string LogFile = "log.txt";
@@ -34,16 +36,17 @@ static void ClearLine(string text, int MoveBack, int xLines = 0) {
         Console.SetCursorPosition(0, Console.CursorTop - xLines);
     }
 }
-static string ArgSwitch(string[] CurrentArg, string LogFile){
+static string ArgSwitch(string[] CurrentArg, string LogFile) {
     int inc = 0;
+    int connectThroughTCPPort = 0;
     Console.WriteLine("TAB to cycle through configurations. ENTER to select:");
 
     while (true){
         ClearLine("", 0);
         Console.Write(CurrentArg[inc]);
-        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        ConsoleKeyInfo keyPress = Console.ReadKey(true);
         
-        if (keyInfo.Key == ConsoleKey.Tab) {
+        if (keyPress.Key == ConsoleKey.Tab) {
             inc += 1;
             ClearLine("", 0);
             Console.SetCursorPosition(0, Console.CursorTop-2);
@@ -53,13 +56,12 @@ static string ArgSwitch(string[] CurrentArg, string LogFile){
             }
             continue;
         }
-        else if (keyInfo.Key == ConsoleKey.Enter) {
-            Console.WriteLine($"\nYou selected to {CurrentArg[inc]}. Continune below.");
+        else if (keyPress.Key == ConsoleKey.Enter) {
+            Console.WriteLine($"\nYou selected to {CurrentArg[inc]}. Continue below.");
 
             while (inc == 0){
                 Console.WriteLine("\nTCP port:");
 
-                int connectThroughTCPPort;
                 bool isValidInput = int.TryParse(Console.ReadLine(), out connectThroughTCPPort);
 
 
@@ -74,28 +76,36 @@ static string ArgSwitch(string[] CurrentArg, string LogFile){
                     break;
                 }
             }
+
+            // Choose TCP port for FE connection
             if (inc == 1)
             {
                 while (inc == 1) {
                     Console.WriteLine("\nIP address:");
-
                     string connectThroughIP = Console.ReadLine();
                     if (string.IsNullOrEmpty(connectThroughIP)) {
                         ClearLine("", 1, 4);
                         Console.WriteLine("No IP address was input, please enter an IP address.");
                     }
                     else {
-                        Console.WriteLine($"\nIP address {connectThroughIP} selected");
+                        Console.WriteLine($"\nIP address {connectThroughIP} selected, press enter to listen on port {connectThroughTCPPort}");
+                        if (connectThroughTCPPort > 0) {
+                            ConnectionPoint(connectThroughTCPPort, CurrentArg);
+                        }
                         Console.WriteLine("\nTAB to cycle through configurations, ENTER to select:");
                         CurrentArg[1] = "Reconfigure IP to use for instrumentDataDB";
                         break;
                     }
                 }
             }
+
+            // Choose IP for DataDB connection
             if (inc == 2)
             {
                 //How the fuck do I make it run in the bg? Minimize maybe?
              }
+
+            // Log to file
             if (inc == 3)
             {
                 File.WriteAllText(LogFile, string.Empty);
@@ -107,5 +117,33 @@ static string ArgSwitch(string[] CurrentArg, string LogFile){
             }
         }
     }
-    return CurrentArg[inc];
+}
+
+static async Task ConnectionPoint(int port, string[] ArgsRotation, string LogFile = "") {                   // Line 122 to 148 was a modification ChatGPT did to my code to make it possible for me to
+    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();                        // cancel the listener with a keypress. This was a huge struggle for me!!! Can't wait to learn more about this!
+
+    Task listeningTask = Task.Run(async () => {
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+
+        while (!cancellationTokenSource.Token.IsCancellationRequested) {
+            TcpClient client = await listener.AcceptTcpClientAsync(cancellationTokenSource.Token);
+
+            if (client.Connected) {
+                Console.WriteLine("Client connected");
+                NetworkStream FE_BE_Stream = client.GetStream();
+                // ...
+            }
+        }
+
+        Console.WriteLine("Stopped listening");
+        listener.Stop();
+    }, cancellationTokenSource.Token);
+
+    Console.WriteLine($"Listening on port {port}");
+    Console.WriteLine("Press Esc to stop listening");
+    while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+
+    cancellationTokenSource.Cancel();
+    await listeningTask;
 }
