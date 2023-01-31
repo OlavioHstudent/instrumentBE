9d300fabaab3b56842b481869be0ebbd0944a24d
OlavioH <olavio.holm@gmail.com>
2023-01-31 16:49:10 +0100

    Added functionality so the user may enter IP and port to connect instrumentBE to instrumentDB. Also added a config that logs to file.
    To do:
    Test to see if I can break the program before I add communication with other instances.

--- instrumentBE/instrumentBEapp.cs
+++ instrumentBE/instrumentBEapp.cs
@@ -1,16 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
+// Variables
+string LogFile = "log.txt";
 string[] ArgsRotation = new string[] {"Configure TCP port to use for FE connections",           // 0
                                       "Configure IP address and tcp port to instrumentDataDB",  // 1
-                                      "Run program in background",                    // 2
-                                      "Log to file"};                                 // 3
+                                      "Run program in background - NOT WORKING - ",             // 2
+                                      "Log to file",                                            // 3
+                                      "This index is here because I can't access index 3 without it somehow :/ "};
 
-ArgSwitch(ArgsRotation);
+// Program
+ArgSwitch(ArgsRotation, LogFile);
 
+
+// Methods
 static void ClearLine(string text, int MoveBack, int xLines = 0) {
 
     if (MoveBack == 0) {
@@ -25,12 +32,12 @@ static void ClearLine(string text, int MoveBack, int xLines = 0) {
             Console.WriteLine(new string(' ', Console.BufferWidth));
         }
         Console.SetCursorPosition(0, Console.CursorTop - xLines);
-
     }
 }
-static string ArgSwitch(string[] CurrentArg){
+static string ArgSwitch(string[] CurrentArg, string LogFile){
     int inc = 0;
-    Console.WriteLine("Press TAB to switch between arguments, ENTER to select");
+    Console.WriteLine("TAB to cycle through configurations. ENTER to select:");
+
     while (true){
         ClearLine("", 0);
         Console.Write(CurrentArg[inc]);
@@ -40,42 +47,69 @@ static string ArgSwitch(string[] CurrentArg){
             inc += 1;
             ClearLine("", 0);
             Console.SetCursorPosition(0, Console.CursorTop-2);
-            if (CurrentArg[inc] == CurrentArg[CurrentArg.Length - 1]) {
+
+            if (CurrentArg[inc] == CurrentArg[CurrentArg.Length-1]) {
                 inc = 0;
             }
             continue;
         }
         else if (keyInfo.Key == ConsoleKey.Enter) {
-            Console.WriteLine($"\nYou selected to {CurrentArg[inc]}");
-            
+            Console.WriteLine($"\nYou selected to {CurrentArg[inc]}. Continune below.");
+
             while (inc == 0){
                 Console.WriteLine("\nTCP port:");
+
                 int connectThroughTCPPort;
                 bool isValidInput = int.TryParse(Console.ReadLine(), out connectThroughTCPPort);
 
+
                 if (!isValidInput) {
                     ClearLine("", 1, 4);
-                    Console.WriteLine("No port number was input, please try again.");
+                    Console.WriteLine("No port number was input, please choose a port.");
                 }
                 else {
-                    CurrentArg[0] = "Reconfigure TCP port to use for FE connections";
+                    Console.WriteLine($"\nPort {connectThroughTCPPort} selected");
+                    Console.WriteLine("\nTAB to cycle through configurations, ENTER to select:");
+                    CurrentArg[0] = "Reconfigure TCP port to use for instrumentFE connections";
                     break;
                 }
             }
             if (inc == 1)
             {
+                while (inc == 1) {
+                    Console.WriteLine("\nIP address:");
 
+                    string connectThroughIP = Console.ReadLine();
+                    if (string.IsNullOrEmpty(connectThroughIP)) {
+                        ClearLine("", 1, 4);
+                        Console.WriteLine("No IP address was input, please enter an IP address.");
+                    }
+                    else {
+                        Console.WriteLine($"\nIP address {connectThroughIP} selected");
+                        Console.WriteLine("\nTAB to cycle through configurations, ENTER to select:");
+                        CurrentArg[1] = "Reconfigure IP to use for instrumentDataDB";
+                        break;
+                    }
+                }
             }
             if (inc == 2)
             {
-
-            }
+                //How the fuck do I make it run in the bg? Minimize maybe?
+             }
             if (inc == 3)
             {
-
+                File.WriteAllText(LogFile, string.Empty);
+                using (StreamWriter LogToFile = File.AppendText("log.txt")) {
+                    LogToFile.WriteLine("Logging this text");
+                }
+                string LogFilePath = Path.Combine(Directory.GetCurrentDirectory(), LogFile);
+                Console.WriteLine($"Sensor Data logged to log.txt in this directory:\n{LogFilePath}");
             }
         }
     }
     return CurrentArg[inc];
 }
 
+bool TryParse(string? v, out string connectThroughTCPPort) {
+    throw new NotImplementedException();
+}
